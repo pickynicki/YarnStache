f393b8edd262690c304b74e97d46b7bfdf91c640
Nicki M <nickim30@gmail.com>
2017-07-16 21:47:56 -0400

    Add project files.

---
+++ YarnStache/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YarnStache.Startup))]
+namespace YarnStache
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
